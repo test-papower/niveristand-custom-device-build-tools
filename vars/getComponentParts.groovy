a7c0956d383e00a11260243292e551a178e06f4f
Donovan Buck <donovan.buck@ni.com>
2019-05-01 14:12:37 -0500

    Roll back component parts due to build flakiness

--- vars/getComponentParts.groovy
+++ vars/getComponentParts.groovy
@@ -1,17 +1,9 @@
 def call() {
    def partMap = [:]
    def tokens = env.JOB_NAME.tokenize("/")
-   def tokenCount = tokens.size()
-
-   def index = -1
-   def parts = ['branch', 'repo', 'organization']
-
-   for(i = 0; i < tokenCount; i++) {
-      if(parts[i]) {
-         partMap[parts[i]] = tokens[index]
-      }
-      index--
-   }
+   partMap['branch'] = tokens[-1]
+   partMap['repo'] = tokens[-2]
+   partMap['organization'] = tokens[-3]
 
    return partMap
 }
