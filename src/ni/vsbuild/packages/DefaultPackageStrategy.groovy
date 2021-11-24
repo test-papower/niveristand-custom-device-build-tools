69c814f5dfad389e133cbbb69e2e79951d32f7de
Donovan Buck <donovan.buck@ni.com>
2021-11-23 21:33:56 -0600

    only build for specified bitness

--- src/ni/vsbuild/packages/DefaultPackageStrategy.groovy
+++ src/ni/vsbuild/packages/DefaultPackageStrategy.groovy
@@ -1,5 +1,7 @@
 package ni.vsbuild.packages
 
+import ni.vsbuild.Architecture
+
 class DefaultPackageStrategy implements PackageStrategy {
 
    def lvVersion
@@ -8,15 +10,30 @@ class DefaultPackageStrategy implements PackageStrategy {
       this.lvVersion = lvVersion
    }
 
-   // Returns all packages with a configuration where
-   // multi_bitness is not defined OR
-   // if multi_bitness is defined, but the current LabVIEW version
-   // does not match any of the versions specified for multi-bitness packaging.
    def filterPackageCollection(packageCollection) {
-      return packageCollection.findAll { !(it.get('multi_bitness')) \
-                                          || ((it.get('multi_bitness_versions')) \
-                                             && !(it.get('multi_bitness_versions').contains(lvVersion.lvRuntimeVersion)))
+      // Get all packages with a configuration where
+      // multi_bitness is not defined OR
+      //    if multi_bitness is defined AND the current LabVIEW version
+      //    does not match any of the versions specified for multi-bitness packaging.
+      def filteredCollection = packageCollection.findAll { !(it.get('multi_bitness')) \
+                                                         || ((it.get('multi_bitness_versions')) \
+                                                            && !(it.get('multi_bitness_versions').contains(lvVersion.lvRuntimeVersion)))
       }
+
+      // Remove all remaining packages with a configuration where
+      // bitness is specified AND
+      //    the current bitness does not match the specified bitness OR
+      //    bitness_versions is specified AND the current version is not found.
+      //
+      // If bitness matches and bitness_versions is not specified, assume bitness
+      // applies to all LabVIEW versions.
+      filteredCollection.removeAll { it.get('bitness') \
+                                    && (Architecture.bitnessToArchitecture(it.get('bitness') as Integer) != lvVersion.architecture \
+                                       || ((it.get('bitness_versions')) \
+                                          && !(it.get('bitness_versions').contains(lvVersion.lvRuntimeVersion))))
+      }
+
+      return filteredCollection
    }
 
    def getOutputDirectory(script, outputDir) {
